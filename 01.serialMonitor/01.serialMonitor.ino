d653e213512cfea49873ff43480031879e83a062
Samuel H <samuel.haszard@education.nsw.gov.au>
2025-08-04 09:39:15 +1000

    Update 01.serialMonitor.ino

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Samuel H
   
   Learning Intention:
   The students will understand the difference between the inbuilt setup and loop methods,
@@ -19,14 +19,26 @@
     https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
 */
 
+static unsigned int myInt = 4;
+
 // The setup function runs once when you press reset or power the board
 void setup() {
-
+  // Setup a serial connection and configure the communication speed
+  Serial.begin(9600);
+  // Debug serial connection by printing a confirmation to the serial monitor
+  Serial.print("Serial monitor is confivured to 9600 baud rate");
 }
 
 // The loop function runs over and over again forever
 void loop() {
-
+  Serial.print("Pin-A0");
+  Serial.print(analogRead(A0));
+  Serial.print(",");
+  Serial.print("Pin-A1");
+  Serial.print(analogRead(A1));
+  Serial.print(",");
+  Serial.print("Pin-A2");
+  Serial.println(analogRead(A2));
 }
 
 
